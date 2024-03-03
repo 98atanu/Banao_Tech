6d6de944321f2d91323e43e56b1e6fec74bc3ae8
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-03-03 15:45:48 +0530

    Website created

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Card from './components/Card'
+import CreateAccount from './components/CreateAccount'
+import Signin from './components/Signin'
+import Slider from './components/Slider'
+import Feedbar from './components/Feedbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Slider/>
+    <Feedbar/>
+    <Card/>
+    <CreateAccount/>
+    <Signin/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Slider.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Slider = () => {
+    return (
+        <div className="container1" style={{zIndex: "1",height:"80vh", background: "url(https://eco-n-tech.com/wp-content/cache/thumb/ec/890ddcd1c2ce5ec_3806x1034.png) center center / cover no-repeat", display: "flex"}}>
+        <div className="container1-text d-flex flex-column justify-content-end align-items-start px-4" style={{height: "100%"}}>
+            <h1 className="text-white">Computer Engineering</h1>
+            <p className="text-white mb-5">142,765 Computer Engineers follow this</p>
+        </div>
+    </div>
+    )
+}
+
+export default Slider
